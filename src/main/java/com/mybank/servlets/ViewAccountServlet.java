5e6f7b6344d36999778e40ccfc06574b624fe403
rohanrode02 <roderohan2@gmail.com>
2025-09-08 23:04:43 +0530

    View account details functionality 2

--- src/main/java/com/mybank/servlets/ViewAccountServlet.java
+++ src/main/java/com/mybank/servlets/ViewAccountServlet.java
@@ -2,46 +2,50 @@ package com.mybank.servlets;
 
 import java.io.IOException;
 import java.util.List;
-import javax.servlet.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import com.mybank.dao.AccountDAO;
 import com.mybank.dao.AccountDAO.AccountDetails;
 import com.mybank.dao.AccountDAO.Transaction;
 
+@WebServlet("/ViewAccountServlet")
 public class ViewAccountServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        int accountId = Integer.parseInt(request.getParameter("accountId"));
+        try {
+            // Get accountId parameter
+            int accountId = Integer.parseInt(request.getParameter("accountId"));
 
-        AccountDAO dao = new AccountDAO();
-        AccountDetails account = dao.getAccountDetails(accountId);
-        List<Transaction> transactions = dao.getTransactionHistory(accountId);
+            // Create DAO instance
+            AccountDAO dao = new AccountDAO();
 
-        response.setContentType("text/html;charset=UTF-8");
-        StringBuilder html = new StringBuilder();
-        html.append("<h2>Account Details</h2>");
-        if (account != null) {
-            html.append("<p>Account ID: ").append(account.getAccountId()).append("</p>");
-            html.append("<p>Account Type: ").append(account.getAccountType()).append("</p>");
-            html.append("<p>Balance: ").append(account.getBalance()).append("</p>");
+            // Fetch account details and transaction history
+            AccountDetails account = dao.getAccountDetails(accountId);
+            List<Transaction> transactions = dao.getTransactionHistory(accountId);
 
-            html.append("<h3>Transaction History</h3>");
-            html.append("<table border='1'><tr><th>Type</th><th>Amount</th><th>Date</th></tr>");
-            for (Transaction t : transactions) {
-                html.append("<tr>")
-                    .append("<td>").append(t.getType()).append("</td>")
-                    .append("<td>").append(t.getAmount()).append("</td>")
-                    .append("<td>").append(t.getDate()).append("</td>")
-                    .append("</tr>");
-            }
-            html.append("</table>");
-        } else {
-            html.append("<p>Account not found!</p>");
+            // Set as request attributes
+            request.setAttribute("account", account);
+            request.setAttribute("transactions", transactions);
+
+            // Forward to JSP for display
+            request.getRequestDispatcher("/viewAccount.jsp").forward(request, response);
+
+        } catch (NumberFormatException e) {
+            response.getWriter().println("<h3 style='color:red;'>Invalid Account ID!</h3>");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("<h3 style='color:red;'>Error: " + e.getMessage() + "</h3>");
         }
+    }
 
-        response.getWriter().println(html.toString());
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // For simplicity, call doGet
+        doGet(request, response);
     }
 }
